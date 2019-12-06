588b965b89eaeaf0a261a0e7dd6fd7f2932a3ea3
Ewy <EwyBoy@gmail.com>
2019-12-06 20:06:55 +0100

    idk

--- src/main/java/com/ewyboy/cultivatedtech/client/GeneratorTESR.java
+++ src/main/java/com/ewyboy/cultivatedtech/client/GeneratorTESR.java
@@ -3,25 +3,29 @@ package com.ewyboy.cultivatedtech.client;
 import com.ewyboy.bibliotheca.util.ModLogger;
 import com.ewyboy.cultivatedtech.common.content.tile.GeneratorTileEntity;
 import com.mojang.blaze3d.platform.GlStateManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.fluid.Fluids;
 import net.minecraftforge.fluids.FluidStack;
 
 public class GeneratorTESR extends TileEntityRenderer<GeneratorTileEntity> {
 
-    FluidStack fluid = new FluidStack(Fluids.FLOWING_LAVA, 1000);2
+    FluidStack fluid = new FluidStack(Fluids.FLOWING_LAVA, 1000);
 
     private void renderFluid(GeneratorTileEntity te) {
+        System.out.println(te);
+
         if (te != null) {
             GlStateManager.pushMatrix();
             GlStateManager.enableBlend();
-                    FluidRenderHelper.renderSidedFluidCuboid(new FluidStack(Fluids.FLOWING_LAVA, 1000), te.getPos(),
+                    FluidRenderHelper.renderSidedFluidCuboid(fluid, te.getPos(),
                             0.375, 0.0d, 0.15d,
                             0.0d, 0.0625d, 0.0d,
                             0.25, 0.5d, 0.7d,
                             true, true, false, false, false, false
                     );
-                    FluidRenderHelper.renderSidedFluidCuboid(new FluidStack(Fluids.FLOWING_LAVA, 1000), te.getPos(),
+                    FluidRenderHelper.renderSidedFluidCuboid(fluid, te.getPos(),
                             0.15d, 0.0d, 0.375,
                             0.0d, 0.0625d, 0.0d,
                             0.7d, 0.5d, 0.25d,
